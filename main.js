7a8e7b0d0ee456e8d969133275a0fbfbc32081fc
akshayvinchurkar <akshay.vinchurkar@sarpstechnologies.in>
2018-05-08 11:03:25 +0530

    access node power in html file and record the cpu model and cpu speed with os.cpus method

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
-var form = document.querySelector("form");
-var myvalue = document.getElementById("name");
+const electron = require('electron');
+const os = require('os');
+// use ipcRenderer to access All node and electron methods in html file
+const {ipcRenderer} = electron;
 
-form.onsubmit = (e) => {
-    e.preventDefault();
-    console.log(myvalue.value);
-}
+// create ul, li and assign cpus function to normal variable
+const ul = document.getElementById('list');
+let cpus = os.cpus();
+
+// simply get All cpus and iterate on it
+cpus.forEach(cpu => {
+    const li = document.createElement('li');
+    let content = document.createTextNode(`CPU MODEL : ${cpu.model} CPU SPEED : ${cpu.speed} \n`);
+    li.appendChild(content);
+    ul.appendChild(li);
+});
