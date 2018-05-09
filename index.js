f45aea08093de9de82a99ce8769bb70c82800c3a
akshayvinchurkar <akshay.vinchurkar@sarpstechnologies.in>
2018-05-09 18:13:21 +0530

    add new css and remove unnessasary content form html

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ const url = require('url')
 
 const createWindow = () => {
     // Create the browser window.
-    win = new BrowserWindow({ width: 800, height: 600, title:'Akshay' })
+    win = new BrowserWindow({ 
+        width: 800, 
+        height: 600, 
+        title:'Akshay',
+        icon: 'img/logo.png' 
+    });
 
     // and load the index.html of the app.
     win.loadURL(url.format({
