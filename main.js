1c213fc07120fc6544cc30131cdf635cc6d6fe6a
akshayvinchurkar <akshay.vinchurkar@sarpstechnologies.in>
2018-05-11 10:59:56 +0530

    create file i know ui is not good

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 const electron = require('electron');
 const os = require('os');
+const fs = require('fs');
+const path = require('path');
+
 // use ipcRenderer to access All node and electron methods in html file
 const {ipcRenderer} = electron;
 
@@ -13,4 +16,20 @@ cpus.forEach(cpu => {
     let content = document.createTextNode(`CPU MODEL : ${cpu.model} CPU SPEED : ${cpu.speed} \n`);
     li.appendChild(content);
     ul.appendChild(li);
+});
+
+// create all variables
+fileName = document.getElementById('fileName');
+fileContent = document.getElementById('fileContent');
+createFile = document.getElementById('createfile');
+pathName = path.join(__dirname, 'Files');
+
+createFile.addEventListener('click', (e) => {
+    e.preventDefault();
+    let file = path.join(pathName, fileName.value);
+    let content = fileContent.value;
+    fs.writeFile(file, content, (err) => {
+        if (err) throw err;
+        console.log('file is created');
+    });
 });
